7b4883a56e2cbe35ac2120724e4b8f4628a39e35
sandeep <sandeep.ariseup@gmail.com>
2023-08-17 18:11:18 +0530

    Initial commit

---
+++ src/main/java/com/brsbooking/BrsBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.brsbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrsBookingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BrsBookingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/brsbooking/BrsBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brsbooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrsBookingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
