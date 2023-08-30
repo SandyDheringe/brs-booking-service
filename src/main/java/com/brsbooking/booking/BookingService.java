adb6db1f76205811689c8550fb3384f9c2af5391
sandeep <sandeep.ariseup@gmail.com>
2023-08-30 12:25:38 +0530

    cancel booking and api resilience

--- src/main/java/com/brsbooking/booking/BookingService.java
+++ src/main/java/com/brsbooking/booking/BookingService.java
@@ -10,13 +10,17 @@ import com.brsbooking.messages.MessageDestinationConst;
 import com.brsbooking.search.BusRoute;
 import com.brsbooking.search.BusRouteRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.*;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
@@ -41,30 +45,60 @@ public class BookingService {
 
     private final ObjectMapper objectMapper;
 
+    private final RestTemplate restTemplate;
+
+    private static final String INVENTORY_SERVICE = "inventoryService";
+
+
     @Autowired
     BookingService(WebClient.Builder webClientBuilder, BookingRepository bookingRepository,
-                   BusRouteRepository busRouteRepository, PassengerRepository passengerRepository, MessageBroker messageBroker, ObjectMapper objectMapper) {
+                   BusRouteRepository busRouteRepository, PassengerRepository passengerRepository,
+                   MessageBroker messageBroker, ObjectMapper objectMapper, RestTemplate restTemplate) {
         this.webClientBuilder = webClientBuilder;
         this.bookingRepository = bookingRepository;
         this.busRouteRepository = busRouteRepository;
         this.passengerRepository = passengerRepository;
         this.messageBroker = messageBroker;
         this.objectMapper = objectMapper;
+        this.restTemplate = restTemplate;
     }
 
-    public Mono<BusInventoryDto> fetchBusInventory(Integer busId) {
+    //    @CircuitBreaker(name =INVENTORY_SERVICE,fallbackMethod = "fetchDefaultBusInventory")
+//    @Retry(name = INVENTORY_SERVICE,fallbackMethod = "fetchDefaultBusInventory")
+    public Optional<BusInventoryDto> fetchBusInventory(Integer busId) {
+        System.out.println("API call fetchBusInventory");
         return webClientBuilder
                 .baseUrl(INVENTORY_HOST)
                 .build()
                 .get()
                 .uri(GET_INVENTORIES, busId)
                 .retrieve()
-                .bodyToMono(BusInventoryDto.class);
+                .bodyToMono(BusInventoryDto.class).blockOptional();
+    }
+
+    @CircuitBreaker(name = INVENTORY_SERVICE, fallbackMethod = "fetchDefaultBusInventory")
+//    @Retry(name = INVENTORY_SERVICE, fallbackMethod = "fetchDefaultBusInventory")
+    public Optional<BusInventoryDto> fetchBusInventoryOld(Integer busId) {
+        System.out.println("API call fetchBusInventory");
+        HttpHeaders headers = new HttpHeaders();
+        headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+        HttpEntity<BusInventoryDto> entity = new HttpEntity<>(headers);
+        ResponseEntity<BusInventoryDto> res = restTemplate.exchange(INVENTORY_HOST + GET_INVENTORIES,
+                HttpMethod.GET, entity, BusInventoryDto.class, busId);
+        if (res.getStatusCode() == HttpStatus.OK && res.hasBody()) {
+            return Optional.ofNullable(res.getBody());
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    public Optional<BusInventoryDto> fetchDefaultBusInventory(Exception e) {
+        return Optional.empty();
     }
 
     public BookingResponseDto doBusBooking(BookingRequestDto bookingRequestDto) {
 
-        Optional<BusInventoryDto> busInventoryDto = fetchBusInventory(bookingRequestDto.getBusId()).blockOptional();
+        Optional<BusInventoryDto> busInventoryDto = fetchBusInventory(bookingRequestDto.getBusId());
 
         if (busInventoryDto.isPresent()) {
             BusInventoryDto busInventoryDetail = busInventoryDto.get();
@@ -111,7 +145,7 @@ public class BookingService {
             Booking booking = bookingDetail.get();
             booking.setBookingStatus(BookingStatus.CANCEL);
             bookingRepository.saveAndFlush(booking);
-            messageBroker.sendBookingMessage(MessageDestinationConst.DEST_INITIATE_PAYMENT_REFUND,new BookingMessage(bookingId,null));
+            messageBroker.sendBookingMessage(MessageDestinationConst.DEST_INITIATE_PAYMENT_REFUND, new BookingMessage(bookingId, null));
         } else {
             throw new BRSResourceNotFoundException(String.format("No Confirm Booking details with id %d found", bookingId));
         }
--- src/main/java/com/brsbooking/config/AppConfiguration.java
+++ src/main/java/com/brsbooking/config/AppConfiguration.java
@@ -1,9 +1,15 @@
 package com.brsbooking.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class AppConfiguration {
 
+    @Bean
+    public RestTemplate getRestTemplate() {
+        return new RestTemplate();
+    }
 
 }
